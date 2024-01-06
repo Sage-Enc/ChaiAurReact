fb27e943231f5d2c40568aea5af0cf91a38b2bef
Sage <rutveek21.rm@gmail.com>
2024-01-06 11:07:47 +0530

    Done with Tutotial2

---
+++ ReactTut_1/src/App.jsx
@@ -0,0 +1,36 @@
+function App() {
+  return (
+    <div>
+      <h1>Chai Aur React | Sage</h1>
+
+      <h3>Learnings From Tutorial 1:-</h3>
+      <br />
+
+      <ul>
+        <li>What is NPM and NPX</li>
+        <li>How To Create a React Porject Using both CRA and Vite</li>
+        <li>react and react-dom are the main library in the dependencies of react</li>
+        <li>What are the scripts and their uses in react package</li>
+      </ul>
+
+      <h3>Create-React-App:</h3>
+      <ul>
+        <li>Takes More Time to Install Compared to Vite</li>
+        <li>Uses Webpack</li>
+        <li>Perform Slower During Development Compared to Vite</li>
+        <li>CRA has limited pure javascript support</li>
+      </ul>
+      <br />
+      <h3>Vite:</h3>
+      <ul>
+        <li>Takes Less Time to Install Compared to CRA</li>
+        <li>Uses it's own native build tools</li>
+        <li>Perform Faster During Development Compared to CRA</li>
+        <li>Vite has Full pure javascript support</li>
+      </ul>
+      <br />      
+    </div>
+  )
+}
+
+export default App
